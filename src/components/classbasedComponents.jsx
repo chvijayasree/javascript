3d38455ba15db2529eae497b3fe6d27c2e87c0f7
unknown <saivarshu.22>
2021-02-06 11:56:33 +0530

    Initialize project using Create React App

---
+++ src/components/classbasedComponents.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class ClassBasedComponent extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            count:0
+        }
+    }
+    inc=()=>{
+        return this.setState({count:this.state.count+1}
+            )
+    }
+
+
+render(){
+    return(
+         <>
+            <h1>this is {this.props.name} from {this.props.children }</h1>
+            <button onClick={()=>{this.inc()}}>Inc</button>
+            <h1>count is{this.state.count}</h1>
+          </>
+    );
+}
+}
+
+export default ClassBasedComponent;
---
+++ src/components/demo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// function Demo(){
+//     return(
+//         <h1>This is a Demo 
+//     )
+
+const Demo=()=>{
+    return(
+        <h1>this is my first Component. Component name is DEMO</h1>
+        
+    );
+
+}
+
+let Home=()=>{
+    return(
+        <h1>this is my home component</h1>
+    );
+}
+
+
+export default Demo;
+
